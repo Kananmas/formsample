d7feffd6f4616e833ec655f16cb401b123ad9e3c
Kananmas <99276326+Kananmas@users.noreply.github.com>
2023-01-25 15:18:06 +0330

    Delete src directory

--- src/components/Form/components/Alertform/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Alert } from "@mantine/core";
-
-export default function Alertform(props) {
-  return (
-    <Alert color="red" className="form-alert">
-      {props.text}
-    </Alert>
-  );
-}
